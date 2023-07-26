a8b12ece552f26524b121d901d769435223c6449
Matteo Visentin <visentinmatt@gmail.com>
2023-07-26 13:13:15 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -15,53 +15,10 @@ const Directors = Models.Direcotr;
 
 mongoose.connect('mongodb://localhost:27017/myFlix', { useNewUrlParser: true, useUnifiedTopology: true});
 
-// let users = [
-//   {
-//     "Id": 1,
-//     "Name": "User1",
-//     "Favorites": []
-//   },
-//   {
-//     "Id": 2,
-//     "Name": "User2",
-//     "Favorites": ["A Marrige Story"]
-//   }
-// ]
-
-// let movies = [
-//   {
-//       "Title": "Eternal Sunshine Of A Spottles Mind",
-//       "Description": "Description",
-//       "Genre": {
-//           "Name": "Name",
-//           "Description": "Description"
-//       },
-//       "Director": {
-//           "Name": "Director's name",
-//           "Bio": "Bio",
-//           "Born": "Date of Birth",
-//           "Dead": "Date of death"
-//       },
-//       "Year": "Year",
-//       "Image": "#"
-//   },
-//   {
-//       "Title": "A Marrige Story",
-//       "Description": "Description",
-//       "Genre": {
-//           "Name": "Name",
-//           "Description": "Description"
-//       },
-//       "Director": {
-//           "Name": "Director's name2",
-//           "Bio": "Bio",
-//           "Born": "Date of Birth",
-//           "Dead": "Date of death"
-//       },
-//       "Year": "Year",
-//       "Image": "#"
-//   }
-// ];
+// Default
+app.get('/', (req, res) => {
+  res.send('Welcome to myFlix!');
+});
 
 // READ Get all movies
 app.get('/movies', async (req,res) => {
@@ -74,9 +31,6 @@ app.get('/movies', async (req,res) => {
       res.status(500).send('Error: ' + err);
     });
 });
-/*app.get('/movies', (req, res) => {
-  res.status(200).json(movies);
-});*/
 
 // READ Get single movie by Title
 app.get('/movies/:MovieTitle', async (req, res) => {
@@ -89,16 +43,6 @@ app.get('/movies/:MovieTitle', async (req, res) => {
       res.status(500).send('Error: ' + err);
     });
 });
-/*app.get('/movies/:title', (req, res) => {
-  const { title } = req.params;
-  const movie = movies.find( movie => movie.Title === title);
-
-  if (movie) {
-    return res.status(200).json(movie);
-  } else {
-    res.status(400).send('No movie in the list mathces the title you provided!');
-  }
-});*/
 
 // READ Get genre of a movie by its name
 app.get('/movies/genre/:genreName', async (req, res) => {
@@ -123,17 +67,6 @@ app.get('/movies/director/:directorName', async (req, res) => {
       res.status(500).send('Error: ' + err);
     });
 });
-
-/* app.get('/movies/director/:directorName', (req, res) => {
-  const { directorName } = req.params;
-  const director = movies.find( movie => movie.Director.Name === directorName).Director;
-
-  if (director) {
-    return res.status(200).json(director);
-  } else {
-    res.status(400).send('No Director mathces your input! Please try with a different genre.');
-  }
-}); */
 
 // CREAT Post new user
 app.post('/users', async (req, res) => {
@@ -160,17 +93,6 @@ app.post('/users', async (req, res) => {
       res.status(500).send('Error: ' + error);
     });
 });
-/*app.post('/users', (req, res) => {
-  const newUser = req.body;
-
-  if (newUser.Name) {
-    newUser.Id = uuid.v4();
-    users.push(newUser);
-    res.status(201).json(newUser);
-  } else {
-    res.status(400).send('User needs a name!');
-  }
-});*/
 
 // READ Get all users
 app.get('/users', async (req,res) => {
@@ -215,19 +137,6 @@ app.put('/users/:Username', async (req, res) => {
       res.status(500).send('Error: ' + err);
     })
 });
-/*app.put('/users/:userId', (req, res) => {
-  const { userId } = req.params;
-  const updateUser = req.body;
-
-  let user = users.find( user => user.Id == userId);
-
-  if (user) {
-    user.Name = updateUser.Name;
-    res.status(200).json(user);
-  } else {
-    res.status(400).send('User not found');
-  }
-});*/
 
 // POST Add movie to list of favorites
 app.post('/users/:Username/movies/:MovieID', async (req, res) => {
@@ -244,19 +153,6 @@ app.post('/users/:Username/movies/:MovieID', async (req, res) => {
   });
 });
 
-/*app.patch('/users/:userId/favorites/:movieTitle', (req, res) => {
-  const { userId, movieTitle } = req.params;
-
-  let user = users.find( user => user.Id == userId);
-
-  if (user) {
-    user.Favorites.push(movieTitle);
-    res.status(200).send(movieTitle + ' has been succesfully added to your favorites!');
-  } else {
-    res.status(400).send(movieTitle + ' has not been added to your favorite do to an unexpected error.');
-  }
-});*/
-
 // DELETE Remove movie form list of favorites
 app.delete('/users/:Username/movies/:MovieID', async (req, res) => {
   await Users.findOneAndUpdate({ Username: req.params.Username }, {
@@ -271,18 +167,6 @@ app.delete('/users/:Username/movies/:MovieID', async (req, res) => {
     res.status(500).send('Error: ' + err);
   });
 });
-/*app.delete('/users/:userId/favorites/:movieTitle', (req, res) => {
-  const { userId, movieTitle } = req.params;
-
-  let user = users.find( user => user.Id == userId);
-
-  if (user) {
-    user.Favorites = user.Favorites.filter( Title => Title !== movieTitle);
-    res.status(200).send(movieTitle + ' has been succesfully removed from your favorites!');
-  } else {
-    res.status(400).send(movieTitle + ' has not been removed from your favorite do to an unexpected error.');
-  }
-});*/
 
 // DELETE user
 app.delete('/users/:Username', async (req, res) => {
@@ -299,19 +183,6 @@ app.delete('/users/:Username', async (req, res) => {
     res.status(500).send('Error: ' + err);
   });
 });
-
-/*app.delete('/users/:id', (req, res) => {
-  const { id } = req.params;
-
-  let user = users.find( user => user.Id == id);
-
-  if (user) {
-    users = users.filter(user => user.Id != id);
-    res.status(200).send('User ' + user.Id + ' has been succesfully removed!');
-  } else {
-    res.status(400).send(movieTitle + ' has not been removed from your favorite do to an unexpected error.');
-  }
-});*/
 
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');

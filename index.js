4f1478d5fee64d21945cfe240aa4e655b8f6a196
Matteo Visentin <visentinmatt@gmail.com>
2023-07-31 13:50:11 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -5,8 +5,25 @@ const express = require('express'),
       Models = require('./models.js');
 
 const app = express();
+const cors = require('cors');
+const { check, validationResult } = require('express-validator');
+const port = process.env.PORT || 8080;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1){
+      let message = 'The CORS policy for this application doesn\'t allow access from origin: ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require('./auth.js')(app);
 
@@ -72,7 +89,19 @@ app.get('/movies/director/:directorName', passport.authenticate('jwt', { session
 });
 
 // CREAT Post new user
-app.post('/users', async (req, res) => {
+app.post('/users', [
+  check('Username', 'Username is required').isLength({min: 5}),
+  check('Username', 'Username contains non aplhanumeric characters - not allowed').isAlphanumeric(),
+  check('Password', 'Passwor is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail(),
+  check('Birthday', 'Birthday format is incorrect. Please follow this date-format: YYYY-MM-DD').isDate()
+],
+async (req, res) => {
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  let hashedPassword = Users.hashPassword(req.body.Password);
   await Users.findOne({ Username: req.body.Username })
     .then((user) => {
       if (user) {
@@ -80,7 +109,7 @@ app.post('/users', async (req, res) => {
       } else {
         Users.create({
           Username: req.body.Username,
-          Password: req.body.Password,
+          Password: hashedPassword,
           Email: req.body.Email,
           Birthday: req.body.Birthday
         })
@@ -123,26 +152,38 @@ app.get('/users/:Username',  passport.authenticate('jwt', { session: false }), a
 });
 
 // UPDATE Update a user's info, by username
-app.put('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
-    if(req.user.Username !== req.params.Username){
-      return res.status(400).send('Permision denied.')
-    }
-    await Users.findOneAndUpdate({ Username: req.params.Username },
-    { $set:
-    {
-      Username: req.body.Username,
-      Password: req.body.Password,
-      Email: req.body.Email,
-      Birthday: req.body.Birthday
-    }},
-    { new: true})
-    .then((updatedUser) =>{
-      res.status(200).json(updatedUser);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send('Error: ' + err);
-    })
+app.put('/users/:Username', passport.authenticate('jwt', { session: false }), [
+  check('Username', 'Username must be at least 5 characters').isLength({min: 5}),
+  check('Username', 'Username contains non aplhanumeric characters - not allowed').isAlphanumeric(),
+  check('Password', 'Passwor is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail(),
+  check('Birthday', 'Birthday format is incorrect. Please follow this date-format: YYYY-MM-DD').isDate()
+],
+async (req, res) => {
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array });
+  }
+  if(req.user.Username !== req.params.Username){
+    return res.status(400).send('Permision denied.')
+  }
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  await Users.findOneAndUpdate({ Username: req.params.Username },
+  { $set:
+  {
+    Username: req.body.Username,
+    Password: hashedPassword,
+    Email: req.body.Email,
+    Birthday: req.body.Birthday
+  }},
+  { new: true})
+  .then((updatedUser) =>{
+    res.status(200).json(updatedUser);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).send('Error: ' + err);
+  })
 });
 
 // POST Add movie to list of favorites
@@ -200,6 +241,6 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
   });
 });
 
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+app.listen(port, '0.0.0.0.', () => {
+  console.log('Listening port: ' + port);
 });
